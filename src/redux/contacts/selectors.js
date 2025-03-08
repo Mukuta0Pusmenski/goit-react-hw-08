7feae4d86214c1f32814ed0cc54f826fecfc6d58
Mukuta0Pusmenski <167695339+Mukuta0Pusmenski@users.noreply.github.com>
2025-03-08 16:07:16 +0200

    fix=push-4

--- src/redux/contacts/selectors.js
+++ src/redux/contacts/selectors.js
@@ -6,3 +6,16 @@ export const selectLoading = state => state.contacts.loading;
 
 // Вибір помилки
 export const selectError = state => state.contacts.error;
+
+// Додатковий селектор для фільтрованих контактів
+import { createSelector } from '@reduxjs/toolkit';
+import { selectNameFilter } from '../filters/selectors';
+
+export const selectFilteredContacts = createSelector(
+  [selectContacts, selectNameFilter],
+  (contacts, filter) => {
+    return contacts.filter(contact =>
+      contact.name.toLowerCase().includes(filter.toLowerCase())
+    );
+  }
+);
