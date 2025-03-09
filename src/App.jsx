570780e53e85aee5c6e268f670f74bece82dca72
Mukuta0Pusmenski <167695339+Mukuta0Pusmenski@users.noreply.github.com>
2025-03-09 09:11:54 +0200

    sav

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,41 @@
 import React, { useEffect } from 'react';
+import { Routes, Route } from 'react-router-dom';
 import { useDispatch } from 'react-redux';
-import { fetchContacts } from './redux/contactsOps';
-import { Provider } from 'react-redux';
-import store from './redux/store'; 
-import ContactList from './components/ContactList/ContactList.jsx';
-import SearchBox from './components/SearchBox/SearchBox.jsx';
-import ContactForm from './components/ContactForm/ContactForm.jsx';
-import './App.module.css';
+import PrivateRoute from './components/PrivateRoute'; // Створимо цей компонент пізніше
+import RestrictedRoute from './components/RestrictedRoute'; // Створимо цей компонент пізніше
+import Layout from './components/Layout'; // Компонент для загального макета
+import HomePage from './pages/HomePage/HomePage';
+import RegistrationPage from './pages/RegistrationPage/RegistrationPage';
+import LoginPage from './pages/LoginPage/LoginPage';
+import ContactsPage from './pages/ContactsPage/ContactsPage';
+import { refreshUser } from './redux/auth/operations'; // Функція для оновлення користувача
 
 const App = () => {
   const dispatch = useDispatch();
 
   useEffect(() => {
-    dispatch(fetchContacts());
+    dispatch(refreshUser());
   }, [dispatch]);
 
   return (
-    <div className="App">
-      <h1>Phonebook</h1>
-      <ContactForm />
-      <SearchBox />
-      <ContactList />
-    </div>
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<HomePage />} />
+        <Route
+          path="/register"
+          element={<RestrictedRoute component={RegistrationPage} redirectTo="/contacts" />}
+        />
+        <Route
+          path="/login"
+          element={<RestrictedRoute component={LoginPage} redirectTo="/contacts" />}
+        />
+        <Route
+          path="/contacts"
+          element={<PrivateRoute component={ContactsPage} />}
+        />
+      </Route>
+    </Routes>
   );
 };
 
-const AppWrapper = () => (
-  <Provider store={store}>
-    <App />
-  </Provider>
-);
-
-export default AppWrapper;
+export default App;
