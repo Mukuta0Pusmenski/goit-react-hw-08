ac7c2536475f7de9f27a968342932e3810cbca1a
Mukuta0Pusmenski <167695339+Mukuta0Pusmenski@users.noreply.github.com>
2025-03-13 01:55:21 +0200

    push

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,19 @@
 import React, { useEffect } from 'react';
 import { Routes, Route } from 'react-router-dom';
 import { useDispatch } from 'react-redux';
-import PrivateRoute from './components/PrivateRoute'; // Створимо цей компонент пізніше
-import RestrictedRoute from './components/RestrictedRoute'; // Створимо цей компонент пізніше
-import Layout from './components/Layout'; // Компонент для загального макета
+import PrivateRoute from './components/PrivateRoute/PrivateRoute';
+import RestrictedRoute from './components/RestrictedRoute/RestrictedRoute';
+import Layout from './components/Layout/Layout';
 import HomePage from './pages/HomePage/HomePage';
 import RegistrationPage from './pages/RegistrationPage/RegistrationPage';
 import LoginPage from './pages/LoginPage/LoginPage';
 import ContactsPage from './pages/ContactsPage/ContactsPage';
-import { refreshUser } from './redux/auth/operations'; // Функція для оновлення користувача
+import { refreshUser } from './redux/auth/operations'; // Операція для оновлення користувача
 
 const App = () => {
   const dispatch = useDispatch();
 
+  // Під час завантаження застосунку оновлюємо користувача (якщо є токен)
   useEffect(() => {
     dispatch(refreshUser());
   }, [dispatch]);
@@ -20,7 +21,10 @@ const App = () => {
   return (
     <Routes>
       <Route path="/" element={<Layout />}>
+        {/* Головна сторінка */}
         <Route index element={<HomePage />} />
+
+        {/* Публічні маршрути */}
         <Route
           path="/register"
           element={<RestrictedRoute component={RegistrationPage} redirectTo="/contacts" />}
@@ -29,6 +33,8 @@ const App = () => {
           path="/login"
           element={<RestrictedRoute component={LoginPage} redirectTo="/contacts" />}
         />
+
+        {/* Приватний маршрут */}
         <Route
           path="/contacts"
           element={<PrivateRoute component={ContactsPage} />}
