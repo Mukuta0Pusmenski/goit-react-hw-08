ae0cbfb0ddcb4524046e7400f5ac543666662a62
Mukuta0Pusmenski <167695339+Mukuta0Pusmenski@users.noreply.github.com>
2025-03-13 03:14:07 +0200

    push=

--- src/redux/auth/slice.js
+++ src/redux/auth/slice.js
@@ -1,14 +1,49 @@
-import { configureStore } from '@reduxjs/toolkit';
-import authReducer from './auth/slice';
-import contactsReducer from './contacts/slice';
-import filtersReducer from './filters/slice';
+import { createSlice } from '@reduxjs/toolkit';
+import { register, login, logout, refreshUser } from './operations';
 
-export const store = configureStore({
-  reducer: {
-    auth: authReducer,
-    contacts: contactsReducer,
-    filters: filtersReducer,
+const initialState = {
+  user: {
+    name: null,
+    email: null,
+  },
+  token: null,
+  isLoggedIn: false,
+  isRefreshing: false,
+};
+
+const authSlice = createSlice({
+  name: 'auth',
+  initialState,
+  reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(register.fulfilled, (state, action) => {
+        state.user = action.payload.user;
+        state.token = action.payload.token;
+        state.isLoggedIn = true;
+      })
+      .addCase(login.fulfilled, (state, action) => {
+        state.user = action.payload.user;
+        state.token = action.payload.token;
+        state.isLoggedIn = true;
+      })
+      .addCase(logout.fulfilled, (state) => {
+        state.user = { name: null, email: null };
+        state.token = null;
+        state.isLoggedIn = false;
+      })
+      .addCase(refreshUser.pending, (state) => {
+        state.isRefreshing = true;
+      })
+      .addCase(refreshUser.fulfilled, (state, action) => {
+        state.user = action.payload;
+        state.isLoggedIn = true;
+        state.isRefreshing = false;
+      })
+      .addCase(refreshUser.rejected, (state) => {
+        state.isRefreshing = false;
+      });
   },
 });
 
-export default store;
+export default authSlice.reducer;
