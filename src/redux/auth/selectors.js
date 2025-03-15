050d70b11d3c97f03c4cc217b16e1783fff8fc0f
Mukuta0Pusmenski <167695339+Mukuta0Pusmenski@users.noreply.github.com>
2025-03-15 16:25:22 +0200

    fix=push-2

--- src/redux/auth/selectors.js
+++ src/redux/auth/selectors.js
@@ -7,5 +7,4 @@ export const selectIsLoggedIn = state => state.auth.isLoggedIn;
 // Вибір статусу оновлення (refresh)
 export const selectIsRefreshing = state => state.auth.isRefreshing;
 
-// Вибір токена
 export const selectToken = state => state.auth.token;
