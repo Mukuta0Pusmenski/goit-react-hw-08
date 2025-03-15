bc906285b358ed38b9074ce0f2cb3c0ac1bbb4ba
Mukuta0Pusmenski <167695339+Mukuta0Pusmenski@users.noreply.github.com>
2025-03-15 21:04:38 +0200

    push=test-1

--- src/pages/ContactsPage/ContactsPage.jsx
+++ src/pages/ContactsPage/ContactsPage.jsx
@@ -2,27 +2,27 @@ import { useDispatch, useSelector } from 'react-redux';
 import { useEffect } from 'react';
 import { fetchContacts } from '../../redux/contacts/operations';
 import ContactForm from '../../components/ContactForm/ContactForm';
-import SearchBox from '../../components/Filter/Filter'; // Якщо компонент називається "Filter"
+import SearchBox from '../../components/Filter/Filter'; 
 import ContactList from '../../components/ContactList/ContactList';
-import { selectContacts } from '../../redux/contacts/selectors'; // Перевірте правильність шляху
+import { selectContacts } from '../../redux/contacts/selectors'; 
 
 const ContactsPage = () => {
   const dispatch = useDispatch();
-  const contacts = useSelector(selectContacts); // Отримуємо контакти з Redux
+  const contacts = useSelector(selectContacts); 
 
   useEffect(() => {
-    dispatch(fetchContacts()); // Завантажуємо контакти після рендера
+    dispatch(fetchContacts()); 
   }, [dispatch]);
 
   return (
     <div className="App">
       <h1>Phonebook</h1>
       <ContactForm /> {/* Форма для додавання контакту */}
-      <SearchBox /> {/* Поле для пошуку контактів */}
+      <SearchBox /> {/* Поле для пошуку */}
       {contacts.length > 0 ? (
-        <ContactList /> {/* Відображення списку контактів */}
+        <ContactList /> // Відображення списку контактів
       ) : (
-        <p>No contacts available. Add your first contact!</p>
+        <p>No contacts available. Add your first contact!</p> 
       )}
     </div>
   );
