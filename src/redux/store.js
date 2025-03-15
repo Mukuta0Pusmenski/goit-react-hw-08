593321d9594573eaa44ea6e6a85dc4543945f6b9
Mukuta0Pusmenski <167695339+Mukuta0Pusmenski@users.noreply.github.com>
2025-03-15 20:16:33 +0200

    push=fix-3

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,3 +1,12 @@
+import { configureStore } from '@reduxjs/toolkit';
 import authReducer from './auth/slice';
 import contactsReducer from './contacts/slice';
 import filtersReducer from './filters/slice';
+
+export const store = configureStore({
+  reducer: {
+    auth: authReducer, // Відповідає за стан авторизації
+    contacts: contactsReducer, // Відповідає за контакти
+    filters: filtersReducer, // Відповідає за фільтри
+  },
+});
