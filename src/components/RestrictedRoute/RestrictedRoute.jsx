8a3ca90c140f864242418918a2a9c9a267eec4d1
Mukuta0Pusmenski <167695339+Mukuta0Pusmenski@users.noreply.github.com>
2025-03-15 16:19:40 +0200

    fai=push

--- src/components/RestrictedRoute/RestrictedRoute.jsx
+++ src/components/RestrictedRoute/RestrictedRoute.jsx
@@ -1,11 +1,12 @@
+import React from 'react';
 import { Navigate } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import { selectIsLoggedIn } from '../../redux/auth/selectors';
 
-const PrivateRoute = ({ component: Component, ...rest }) => {
+const RestrictedRoute = ({ component: Component, redirectTo, ...rest }) => {
   const isLoggedIn = useSelector(selectIsLoggedIn);
 
-  return isLoggedIn ? <Component {...rest} /> : <Navigate to="/login" />;
+  return isLoggedIn ? <Navigate to={redirectTo} /> : <Component {...rest} />;
 };
 
-export default PrivateRoute;
+export default RestrictedRoute;
