3045dd1189240d8766aea09434e1369683109bb2
Mukuta0Pusmenski <167695339+Mukuta0Pusmenski@users.noreply.github.com>
2025-03-16 10:16:47 +0200

    fix=push-3

--- src/App.jsx
+++ src/App.jsx
@@ -5,11 +5,11 @@ import { refreshUser } from './redux/auth/operations';
 import { selectIsRefreshing } from './redux/auth/selectors';
 import Layout from './components/Layout/Layout';
 import HomePage from './pages/HomePage/HomePage';
-import LoginPage from './pages/LoginPage';
-import RegistrationPage from './pages/RegistrationPage';
-import ContactsPage from './pages/ContactsPage';
-import PrivateRoute from './components/PrivateRoute';
-import RestrictedRoute from './components/RestrictedRoute';
+import LoginPage from './pages/LoginPage/LoginPage';
+import RegistrationPage from './pages/RegistrationPage/RegistrationPage';
+import ContactsPage from './pages/ContactsPage/ContactsPage';
+import PrivateRoute from './components/PrivateRoute/PrivateRoute';
+import RestrictedRoute from './components/RestrictedRoute/RestrictedRoute';
 
 const App = () => {
   const dispatch = useDispatch();
