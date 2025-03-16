98f84e1e781fae4c8a4dd497d46dc458075529c9
Mukuta0Pusmenski <167695339+Mukuta0Pusmenski@users.noreply.github.com>
2025-03-16 10:13:48 +0200

    fix=push-1

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,36 @@
-import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { useEffect } from 'react';
 import { Routes, Route } from 'react-router-dom';
+import { refreshUser } from './redux/auth/operations';
+import { selectIsRefreshing } from './redux/auth/selectors';
 import Layout from './components/Layout/Layout';
-import HomePage from './pages/HomePage/HomePage';
-import LoginPage from './pages/LoginPage/LoginPage';
-import RegistrationPage from './pages/RegistrationPage/RegistrationPage';
-import ContactsPage from './pages/ContactsPage/ContactsPage';
-import PrivateRoute from './components/PrivateRoute/PrivateRoute';
-import RestrictedRoute from './components/RestrictedRoute/RestrictedRoute';
+import HomePage from './pages/HomePage';
+import LoginPage from './pages/LoginPage';
+import RegistrationPage from './pages/RegistrationPage';
+import ContactsPage from './pages/ContactsPage';
+import PrivateRoute from './components/PrivateRoute';
+import RestrictedRoute from './components/RestrictedRoute';
 
-const App = () => (
-  <Routes>
-    <Route path="/" element={<Layout />}>
-      <Route index element={<HomePage />} />
-<Route path="/register" element={<RestrictedRoute component={RegistrationPage} redirectTo="/contacts" />} />
-      <Route path="/login" element={<RestrictedRoute component={LoginPage} redirectTo="/contacts" />} />
-      <Route path="/contacts" element={<PrivateRoute component={ContactsPage} />} />
-    </Route>
-  </Routes>
-);
+const App = () => {
+  const dispatch = useDispatch();
+  const isRefreshing = useSelector(selectIsRefreshing);
+
+  useEffect(() => {
+    dispatch(refreshUser()); // Оновлення користувача при завантаженні програми
+  }, [dispatch]);
+
+  return isRefreshing ? (
+    <p>Loading...</p> // Показуємо спінер чи текст під час завантаження
+  ) : (
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<HomePage />} />
+        <Route path="login" element={<RestrictedRoute component={<LoginPage />} redirectTo="/contacts" />} />
+        <Route path="register" element={<RestrictedRoute component={<RegistrationPage />} redirectTo="/contacts" />} />
+        <Route path="contacts" element={<PrivateRoute component={<ContactsPage />} redirectTo="/login" />} />
+      </Route>
+    </Routes>
+  );
+};
 
 export default App;
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,12 +1,33 @@
 import { configureStore } from '@reduxjs/toolkit';
+import { persistStore, persistReducer, FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 import authReducer from './auth/slice';
 import contactsReducer from './contacts/slice';
 import filtersReducer from './filters/slice';
 
+// Конфігурація для persist
+const authPersistConfig = {
+  key: 'auth',
+  storage,
+  whitelist: ['token'], // Тільки токен зберігається у localStorage
+};
+
+const persistedAuthReducer = persistReducer(authPersistConfig, authReducer);
+
+// Налаштування Store
 export const store = configureStore({
   reducer: {
-    auth: authReducer,
+    auth: persistedAuthReducer,
     contacts: contactsReducer,
     filters: filtersReducer,
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
 });
+
+// Persistor для збереження стану
+export const persistor = persistStore(store);
